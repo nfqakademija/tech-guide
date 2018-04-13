aa4e03e14166bd2ad658f80009a0c923cd53fe87
Matas Šilinskas <silinskas.matas@gmail.com>
2018-04-13 18:53:32 +0300

    dev<Fixtures> Add question fixtures

---
+++ src/DataFixtures/ORM/QuestionProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\DataFixtures\ORM;
+
+use Faker\Provider\Base as BaseProvider;
+use Faker\Generator;
+
+class QuestionProvider extends BaseProvider
+{
+    private $questions = array(
+        'What type of tech would you like to know more about?' => 1,
+        'How often do you travel?' => 10,
+        'How often do you post on social media' => 11,
+        'What`s the color you prefer for your device?' => 101
+    );
+    public function __construct(Generator $generator)
+    {
+        parent::__construct($generator);
+    }
+
+    public function question($question_num)
+    {
+        return array_keys($this->questions)[$question_num - 1];
+    }
+
+    public function priority($question_num)
+    {
+        return array_values($this->questions)[$question_num - 1];
+    }
+}
