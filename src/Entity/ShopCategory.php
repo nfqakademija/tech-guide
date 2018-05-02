2c2d0c7eec60097084df4c1ef4becbef648a1da1
Matas Šilinskas <silinskas.matas@gmail.com>
2018-05-02 15:29:33 +0300

    dev<ShopCategory> add filter columns and fixtures

--- src/Entity/ShopCategory.php
+++ src/Entity/ShopCategory.php
@@ -54,6 +54,36 @@ class ShopCategory
     private $category;
 
     /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $ssdFilter;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $hddFilter;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $processorFilter;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $ramFilter;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $sizeFilter;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $resolutionFilter;
+
+    /**
      * @return int
      */
     public function getId(): int
@@ -198,6 +228,78 @@ class ShopCategory
     public function setCategory(Category $category): self
     {
         $this->category = $category;
+
+        return $this;
+    }
+
+    public function getSsdFilter(): ?string
+    {
+        return $this->ssdFilter;
+    }
+
+    public function setSsdFilter(?string $ssdFilter): self
+    {
+        $this->ssdFilter = $ssdFilter;
+
+        return $this;
+    }
+
+    public function getHddFilter(): ?string
+    {
+        return $this->hddFilter;
+    }
+
+    public function setHddFilter(?string $hddFilter): self
+    {
+        $this->hddFilter = $hddFilter;
+
+        return $this;
+    }
+
+    public function getProcessorFilter(): ?string
+    {
+        return $this->processorFilter;
+    }
+
+    public function setProcessorFilter(?string $processorFilter): self
+    {
+        $this->processorFilter = $processorFilter;
+
+        return $this;
+    }
+
+    public function getRamFilter(): ?string
+    {
+        return $this->ramFilter;
+    }
+
+    public function setRamFilter(?string $ramFilter): self
+    {
+        $this->ramFilter = $ramFilter;
+
+        return $this;
+    }
+
+    public function getSizeFilter(): ?string
+    {
+        return $this->sizeFilter;
+    }
+
+    public function setSizeFilter(?string $sizeFilter): self
+    {
+        $this->sizeFilter = $sizeFilter;
+
+        return $this;
+    }
+
+    public function getResolutionFilter(): ?string
+    {
+        return $this->resolutionFilter;
+    }
+
+    public function setResolutionFilter(?string $resolutionFilter): self
+    {
+        $this->resolutionFilter = $resolutionFilter;
 
         return $this;
     }
