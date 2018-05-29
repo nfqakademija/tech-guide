1955e70ab11c602c4d8f91e441a295989af236de
Matas Šilinskas <silinskas.matas@gmail.com>
2018-05-29 13:58:21 +0300

    dev<Asynchronous> fetch rezults asynchronously

--- src/Utils/HtmlTools.php
+++ src/Utils/HtmlTools.php
@@ -11,6 +11,8 @@ use Doctrine\ORM\EntityManagerInterface;
 
 class HtmlTools
 {
+    private const DATE_DIFF = 3;
+
     /**
      * @var HtmlRepository $htmlRepository
      */
@@ -51,5 +53,28 @@ class HtmlTools
         }
 
         return -1;
+    }
+
+    public function fetchHtmlCode(Shop $shop, string $url) : ?Html
+    {
+        $htmlEntity = $this->htmlRepository->findByUrl($url);
+
+        if ($htmlEntity === null) {
+            try {
+                $pageContent = file_get_contents($url);
+            } catch (\Exception $exception) {
+                return null;
+            }
+            $htmlEntity = $this->htmlRepository->add($shop, $pageContent, $url);
+        } elseif ($htmlEntity->getAddedAt()->diff(new \DateTime('now'))->format('%a') > self::DATE_DIFF) {
+            try {
+                $pageContent = file_get_contents($url);
+            } catch (\Exception $exception) {
+                return null;
+            }
+            $this->htmlRepository->update($htmlEntity, $pageContent);
+        }
+
+        return $htmlEntity;
     }
 }
