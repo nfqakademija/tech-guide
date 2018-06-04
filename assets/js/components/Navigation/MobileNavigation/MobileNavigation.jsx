504cc8f55331e5807a08d426d18115c067e3d20a
Martynas <martynasdd@gmail.com>
2018-06-04 18:36:55 +0300

    fix<responsive_design> fix media queries and add mobile browser toolbar fix

--- assets/js/components/Navigation/MobileNavigation/MobileNavigation.jsx
+++ assets/js/components/Navigation/MobileNavigation/MobileNavigation.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const mobileNavigation = (props) => (
-    <div className="mobile-navigation">
+    <div className="mobile-navigation" style={{height: props.style*0.1}}>
         <button className="mobile-navigation__button mobile-navigation__back" onClick={props.clickedBack} >{props.back}</button>
         <button className="mobile-navigation__button mobile-navigation__next" onClick={props.clickedNext} >{props.next}</button>
     </div>
